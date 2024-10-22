3fb592787560c46ac4b20a30d1296240058eabe2
Khai <khaiwern_3@hotmail.com>
2024-10-22 13:27:18 +0800

    chore: remove test link

--- src/views/AutoReplyBotView.vue
+++ src/views/AutoReplyBotView.vue
@@ -12,9 +12,6 @@ const isEditingNode = computed(() => {
 </script>
 
 <template>
-  <RouterLink :to="{ name: 'auto-reply-bot-with-drawer', params: { id: '1' } }">
-    <button>goto id 1</button>
-  </RouterLink>
   <div class="h-screen w-screen">
     <TheCanvas />
   </div>
